86495e438b6fec86dd428605012a23b6abcc80ab
hpriyaa <67458876+hpriyaa@users.noreply.github.com>
2020-11-28 15:20:33 +0530

    Coursera Project 1
    R code and PNG for the Exploratory Data Analysis course offered by John Hopkins University
    
    ~Haripriya Sivaramakrishnan

---
+++ plot1.r
@@ -0,0 +1,9 @@
+data_File <- "household_power_consumption.txt"
+data <- read.table(data_File, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#plot 1
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
